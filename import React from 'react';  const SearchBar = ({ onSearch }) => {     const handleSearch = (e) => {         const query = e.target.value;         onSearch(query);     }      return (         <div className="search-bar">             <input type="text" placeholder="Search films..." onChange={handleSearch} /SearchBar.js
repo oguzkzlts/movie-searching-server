65b2a85bee2c888921deb68ab64c4d030e235973
oguzkzlts <161623434+oguzkzlts@users.noreply.github.com>
2024-03-22 18:19:51 +0300

    SearchBar added

---
+++ import React from 'react';  const SearchBar = ({ onSearch }) => {     const handleSearch = (e) => {         const query = e.target.value;         onSearch(query);     }      return (         <div className="search-bar">             <input type="text" placeholder="Search films..." onChange={handleSearch} /SearchBar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const SearchBar = ({ onSearch }) => {
+    const handleSearch = (e) => {
+        const query = e.target.value;
+        onSearch(query);
+    }
+
+    return (
+        <div className="search-bar">
+            <input type="text" placeholder="Search films..." onChange={handleSearch} />
+            <button>Search</button>
+        </div>
+    );
+}
+
+export default SearchBar;
